b0928905cba8ddcd6c91d95b296b6e697c4dffa5
JennyAgryrosaSugandi <jennyagryrosa@gmail.com>
2024-05-16 22:46:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot-component>
+      <h2>Data atau Komponen yang Disisipkan</h2>
+      <p>Ini adalah data atau komponen yang akan disisipkan.</p>
+    </slot-component>
+  </div>
+</template>
+
+<script>
+import SlotComponent from './components/SlotComponent.vue';
+
+export default {
+  components: {
+    SlotComponent,
+  },
+};
+</script>
---
+++ src/components/NestedComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h2>Nested Component</h2>
+      <button @click="emitData">Kirim Data ke Parent</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      emitData() {
+        this.$emit('data-emitted', 'Data yang dikirim dari nested component');
+      },
+    },
+  };
+  </script>
+  
